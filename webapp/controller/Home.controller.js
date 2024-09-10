e4552f336727d931fc79d83605391dd1ce25260a
Alexandre Junior <alexandre.junior@lab2dev.com>
2024-09-10 13:21:54 +0000

    feat: create project

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.lab2dev.firstapp.controller.Home", {
+        onInit: function () {
+
+        }
+    });
+});
