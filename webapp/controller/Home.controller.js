70189343d245ba923555eea32d4c8855a69b419e
Alexandre Junior <alexandre.junior@lab2dev.com>
2024-09-10 14:14:30 +0000

    feat: add message toast

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
-function (Controller) {
+function (Controller,MessageToast) {
     "use strict";
 
     return Controller.extend("com.lab2dev.firstapp.controller.Home", {
         onInit: function () {
+            console.log('Hello world!')
+        },
+        onPress: function (oEvent) {
+            // Origem do evento
+            const item = oEvent.getSource();
 
+            // Titulo do item
+            const title = item.getTitle();
+
+            // Mensagem a ser exibida
+            const message = `O item: "${title}" foi clicado!`
+
+            // Exibe uma mensagem na tela
+            MessageToast.show(message);
         }
     });
 });
